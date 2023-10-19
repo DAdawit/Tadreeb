153a8e30085b905d4f54aab4e374759aed11c2b1
Dawit <dawitccnt@gmail.com>
2023-10-19 22:43:29 +0300

    nav complete

--- common/NavBar.tsx
+++ common/NavBar.tsx
@@ -1,31 +1,32 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
   return (
     <nav className="">
       <div className="px-28 flex justify-between items-centers py-3">
-        <div className="w-max">
+        <div className="flex items-end">
           <Image
             src="/logo-tadreeb-1.png"
             alt="logo"
             width={1000}
             height={1000}
-            className="h-24 w-32 object-contain"
+            className="h-16 w-24 xxl:h-24 xxl:w-32 object-contain"
           />
         </div>
         <div className="flex justify-end">
-          <div className="flex flex-col justify-end">
-            <div className="flex gap-x-3 w-max justify-end">
-              <div className="flex items-center justify-center">
+          <div className="flex flex-col justify-end gap-y-4">
+            <div className="flex  justify-end w-full items-center gap-x-3">
+              <div className="flex items-center justify-end">
                 <Image
                   src="/tadreeb-1.png"
                   alt="logo"
                   width={1000}
                   height={1000}
-                  className="h-11 w-11"
+                  className="h-8 w-8 xxl:h-11 xxl:w-11 object-contain"
                 />
-                <h4 className="font-roboto text-2xl text-textPrimary">
+                <h4 className="font-roboto text-sm  xxl:text-2xl text-textPrimary">
                   about us
                 </h4>
               </div>
@@ -35,9 +36,9 @@ const NavBar = () => {
                   alt="logo"
                   width={1000}
                   height={1000}
-                  className="h-11 w-11"
+                  className="h-8 w-8 xxl:h-11 xxl:w-11 object-contain"
                 />
-                <h4 className="font-roboto text-2xl text-textPrimary">
+                <h4 className="font-roboto text-sm  xxl:text-2xl text-textPrimary">
                   +971 000 0000
                 </h4>
               </div>
@@ -47,44 +48,59 @@ const NavBar = () => {
                   alt="logo"
                   width={1000}
                   height={1000}
-                  className="h-11 w-11"
+                  className="h-8 w-8 xxl:h-11 xxl:w-11 object-contain"
                 />
-                <h4 className="font-roboto text-2xl text-textPrimary">
+                <h4 className="font-roboto text-sm  xxl:text-2xl text-textPrimary">
                   contact us
                 </h4>
               </div>
             </div>
-            <div className="flex gap-x-3 w-min justify-end">
-              <Image
-                src="/image-18.png"
-                alt="logo"
-                width={1000}
-                height={1000}
-                className="h-14 object-contain"
-              />
-              <Image
-                src="/image 19.png"
-                alt="logo"
-                width={1000}
-                height={1000}
-                className="h-14 object-contain"
-              />
-              <Image
-                src="/image 20.png"
-                alt="logo"
-                width={1000}
-                height={1000}
-                className="h-14 object-contain"
-              />
-              <Image
-                src="/image 21.png"
-                alt="logo"
-                width={1000}
-                height={1000}
-                className="h-14 object-contain"
-              />
+            <div className="flex gap-x-3 w-min justify-end items-center">
+              {[
+                "/image-18.png",
+                "/image 19.png",
+                "/image 20.png",
+                "/image 21.png",
+              ].map((item, index) => (
+                <div className="w-max h-min" key={index}>
+                  <Image
+                    src={`${item}`}
+                    alt="logo"
+                    width={1000}
+                    height={1000}
+                    className="h-12 w-36 xxl:h-14 xxl:w-56 object-fill"
+                  />
+                </div>
+              ))}
             </div>
           </div>
+        </div>
+      </div>
+      <div className="px-28 flex items-center h-12 xxl:h-16">
+        <div className="bg-secondary flex w-full justify-around items-center h-full">
+          <Link href="#" className="text-white">
+            course finder
+          </Link>
+          <Link href="#" className="text-white">
+            training calendar
+          </Link>
+          <Link href="#" className="text-white">
+            training courses
+          </Link>
+          <Link href="#" className="text-white">
+            venues
+          </Link>
+          <Link href="#" className="text-white">
+            certification
+          </Link>
+          <Link href="#" className="text-white">
+            training type
+          </Link>
+        </div>
+        <div className="bg-primary h-full flex items-center justify-center px-3">
+          <button className="text-white uppercase text-lg font-medium">
+            download
+          </button>
         </div>
       </div>
     </nav>
