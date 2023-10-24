16f8b9fc705da2aa28a0ddc1dc9ada0a1cd2821a
Dawit <dawitccnt@gmail.com>
2023-10-24 22:40:25 +0300

    hero sectiona and search component for category page

---
+++ app/courseCategory/page.tsx
@@ -0,0 +1,12 @@
+import CategoryHero from "@/components/Category/CategoryHero";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <CategoryHero />
+    </div>
+  );
+};
+
+export default page;
