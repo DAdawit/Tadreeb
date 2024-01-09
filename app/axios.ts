43691cee2c7ea71cff1c7f211fd6cb8d7c1e7208
Dawit Diriba <58243647+DAdawit@users.noreply.github.com>
2024-01-09 10:10:22 +0300

    Update axios.ts

--- app/axios.ts
+++ app/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 export const devBaseurl = "http://127.0.0.1:8000/api";
 export const prodBaseUrl = "https://tadreeb.mozivol.com/api";
+export const prodBaseUrl2 = "https://admin.tadreebtc.com/api";
 const api = axios.create({
   // baseURL: "http://127.0.0.1:8000/api",
-  baseURL: prodBaseUrl,
+  baseURL: prodBaseUrl2,
 });
 let token = null;
 if (typeof window !== "undefined") {
