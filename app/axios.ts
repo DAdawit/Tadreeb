c62ce7d6054584982102a99d9590ec3070d762c8
Dawit Diriba <58243647+DAdawit@users.noreply.github.com>
2024-02-05 09:06:56 +0300

    changes

--- app/axios.ts
+++ app/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const devBaseurl = "http://127.0.0.1:8000/api";
-export const prodBaseUrl = "https://tadreeb.mozivol.com/api";
+export const prodBaseUrl = "https://admin.tadreebtc.com/api";
+// export const prodBaseUrl = "https://tadreeb.mozivol.com/api";
 const api = axios.create({
   // baseURL: "http://127.0.0.1:8000/api",
   baseURL: prodBaseUrl,
